4b450ada491ceab8384486f9a9f069082d88bc0f
Hussein Yahya Al-kaf <hyalkaf@Husseins-MacBook-Air.local>
2015-06-13 18:54:05 -0600

    Initial commit

---
+++ app/src/main/java/com/example/hyalkaf/myappportfolio/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.hyalkaf.myappportfolio;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessageSpotifyStreamer(View view) {
+        // Display a Toast message for spotify streamer
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my spotify streamer app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessageScoresApp(View view) {
+        // Display a Toast message for scores app
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my scores app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessageLibraryApp(View view) {
+        // Display a Toast message for library app
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my library app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessageBuildItBigger(View view) {
+        // Display a Toast message for build it bigger
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my build it bigger app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessageBaconReader(View view) {
+        // Display a Toast message for bacon reader
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my bacon reader app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessageCapstone(View view) {
+        // Display a Toast message for capstone
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my capstone app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+}
